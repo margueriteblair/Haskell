0c44f526b3b4c8299a96b4a580f70fb41f456900
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-21 00:52:47 -0500

    basic arithmetic functions in haskell

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,14 @@ module Main where
 
 import Lib
 
+-- interactive shell command: ghci
+-- you'll see prelude and know you're in
+
 main :: IO ()
 main = someFunc
+
+div 5 5 -- will return one, this is a simple integer division function
+--the above is the prefix manner
+--below is the infix manner, sandwiched between two
+5 `div` 5
+succ 7 --will return 8
