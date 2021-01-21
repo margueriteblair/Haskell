cab256517237413534edc5a3b304864c0b113fb3
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-21 01:17:24 -0500

    more function to convert usd back to ada supposing an 8 cent ada

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,7 @@ import Lib
 main :: IO ()
 main = someFunc
 
--- will return one, this is a simple integer division function
+div 5 5 -- will return one, this is a simple integer division function
 --the above is the prefix manner
 --below is the infix manner, sandwiched between two
 5 `div` 5
@@ -16,3 +16,5 @@ succ 7 --will return 8
 True
 False
 adaToUSD ada = ada * 0.08
+usdToADA usd = usd / (8 / 100) -- the '/' function required arguments whose type is in the class fractional
+--div function requires integers
