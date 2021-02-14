064227088bdf356a1b4e298a5e56ba36ec385fe8
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-14 01:04:17 -0500

    imports

---
+++ GuessingGame/GuessingGame.hs
@@ -0,0 +1,11 @@
+import           Control.Monad             (void)
+import qualified Data.ByteString.Char8     as C
+import           Language.Plutus.Contract
+import qualified Language.PlutusTx         as PlutusTx
+import           Language.PlutusTx.Prelude hiding (pure, (<$>))
+import           Ledger                    (Address, Validator, ValidatorCtx, Value, scriptAddress, pubKeyHash)
+import qualified Ledger.Constraints        as Constraints
+import qualified Ledger.Typed.Scripts      as Scripts
+import           Playground.Contract
+import qualified Prelude
+import           Wallet.Emulator.Wallet    (Wallet, walletPubKey)
