b04e15f719a958e3bf42d952278bd6fa5c4487ac
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-14 01:08:00 -0500

    fleshed out game where the w2 user will try and guess the secret number

--- GuessingGame/GuessingGame.hs
+++ GuessingGame/GuessingGame.hs
@@ -18,3 +18,45 @@ magicNum = 123
 
 validateGuess :: Integer -> Integer -> ValidatorCtx -> Bool
 validateGuess actual guess _ = guess == actual || guess == magicNum
+
+gameAddress :: Address
+gameAddress = Ledger.scriptAddress $ Scripts.validatorScript gameInstance
+
+data LockParams = LockParams
+    { amount :: Value}
+    deriving stock (Prelude.Eq, Prelude.Show, Generic)
+    deriving anyclass (FromJSON, ToJSON, IotsType, ToSchema, ToArgument)
+
+
+newtype GuessParams = GuessParams
+    { guessNumber :: Integer}
+    deriving stock (Prelude.Eq, Prelude.Show, Generic)
+    deriving anyclass (FromJSON, ToJSON, IotsType, ToSchema, ToArgument)
+
+
+lock :: AsContractError e => Contract GameSchema e ()
+lock = do
+    LockParams amount <- endpoint @"lock" @LockParams
+    let tx            = Constraints.mustPayToTheScript randomNum amount
+    void $ submitTxConstraints gameInstance tx
+
+
+guess :: AsContractError e => Contract GameSchema e ()
+guess = do
+    GuessParams theGuess <- endpoint @"guess" @GuessParams
+    if theGuess > randomNum
+        then logInfo $ show "It's lower."
+        else if theGuess < randomNum
+            then logInfo $ show "It's higher."
+            else logInfo $ show "Congrats."
+
+    unspentOutputs <- utxoAt gameAddress
+    let tx = collectFromScript unspentOutputs theGuess
+    void $ submitTxConstraintsSpending gameInstance unspentOutputs tx
+
+-- the script is taking two parameter DatumType which is a wrapper around the data we used
+-- in the script output reedemerType is the wrapper around data we take as an input
+data GameDataType
+instance Scripts.ScriptType GameDataType where
+    type instance DatumType GameDataType = Integer
+    type instance RedeemerType GameDataType = Integer
