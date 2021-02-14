38962c94d0ca5817fef26ccc679309effa48d195
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-13 21:23:59 -0500

    whitepsacing matters kids

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 main :: IO ()
-main = do c <- getChar
+main = in_range
 
 in_range :: Integer -> Integer -> Integer -> Bool
 in_range min max x = x >= min && x <= max
 in_range 0 5 3
+
+in_range2 :: Integer -> Integer -> Integer -> Bool
+in_range min max x = if ilb then iub else False
+    where 
+        ilb = min <= x
+        iub = max >= x
