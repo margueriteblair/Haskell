accc923ffe8a7ba977263fb74a4f52d8b7b3df4d
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-15 15:43:19 -0500

    list

--- Lists2.hs
+++ Lists2.hs
@@ -5,3 +5,10 @@
 elem :: (Eq a) => a -> [a] -> Bool
 elem _ [] = False
 elem e (x:xs) = (e == x) || (elem e xs)
+
+--2. Create a function nub that removes all duplicates from a given list
+nub :: (Eq a) => [a] -> [a]
+nub [] : []
+nub (x:xs)
+    | x `elem` xs = nub xs
+    | otherwise = x : nub xs
