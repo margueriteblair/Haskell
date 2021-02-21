e9fd7762417920dc246ba4ca498fe876c10dc1bf
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-20 20:01:48 -0500

    basics on forming lists in haskell

--- app/Lists.hs
+++ app/Lists.hs
@@ -1,3 +1,8 @@
 main :: IO ()
 main = do
     print (1 : 2 : 3 : 4 : 5 : [])
+
+--A list is a particular ordering of elements
+--Every element has a fixed position
+--Lists only have one internal type, we can't have mixed type lists
+
