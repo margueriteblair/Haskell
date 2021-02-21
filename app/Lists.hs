f5403ed66e1cb82e9b4624b8ea6c9833faee68a1
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-20 20:05:38 -0500

    lists

--- app/Lists.hs
+++ app/Lists.hs
@@ -6,3 +6,12 @@ main = do
 --Every element has a fixed position
 --Lists only have one internal type, we can't have mixed type lists
 
+sum :: [Int] -> Int
+sum [] = 0
+sum (x:xs) = x + sum xs
+
+evens :: [Int] -> [Int]
+evens [] = []
+evens (x:xs)
+    | mod x 2 == 0 = x: evens xs
+    | otherwise = evens xs
