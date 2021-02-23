ea9154a06c9b40f428a943572b814813de793c2e
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-23 11:07:28 -0500

    elem refactored

--- Lists2.hs
+++ Lists2.hs
@@ -3,14 +3,17 @@
     --a given list and returns false otherwise
 
 elem :: (Eq a) => a -> [a] -> Bool
-elem _ [] = False
-elem e (x:xs) = (e == x) || (elem e xs)
+elem n (x:xs)
+    | n == x = True
+    | otherwise = Main.elem n xs
+-- elem _ [] = False
+-- elem e (x:xs) = (e == x) || (elem e xs)
 
 --2. Create a function nub that removes all duplicates from a given list
 nub :: (Eq a) => [a] -> [a]
 nub [] = []
 nub (x:xs)
-    | x `elem` xs = nub xs
+    | x `Main.elem` xs = nub xs
     | otherwise = x : nub xs
 
 --3. Create a function isAsc that returns True if the list given to it is a list of ascending order
