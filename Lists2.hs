9effd64cd7a6d0c7e7757a9ffabbe1a14df12433
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-23 11:13:51 -0500

    second list

--- Lists2.hs
+++ Lists2.hs
@@ -1,13 +1,19 @@
 --List exercises
 --1. Create a function elem that returns True if an element is in
     --a given list and returns false otherwise
+main :: IO()
+main = do
+    print (Main.elem 1 [])
 
 elem :: (Eq a) => a -> [a] -> Bool
+elem _ [] = False
 elem n (x:xs)
     | n == x = True
     | otherwise = Main.elem n xs
--- elem _ [] = False
--- elem e (x:xs) = (e == x) || (elem e xs)
+
+elem2 :: (Eq a) => a -> [a] -> Bool
+elem2 _ [] = False
+elem2 e (x:xs) = (e == x) || (elem2 e xs)
 
 --2. Create a function nub that removes all duplicates from a given list
 nub :: (Eq a) => [a] -> [a]
@@ -17,6 +23,6 @@ nub (x:xs)
     | otherwise = x : nub xs
 
 --3. Create a function isAsc that returns True if the list given to it is a list of ascending order
-isAsc :: [Int] -> Bool
+-- isAsc :: [Int] -> Bool
 --4. 
 --four
