7e1a45d292140c06bdc9565c0e67b11e1fc29dcf
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-24 17:14:48 -0500

    lesson on higher order functions

---
+++ app/HigherOrder.hs
@@ -0,0 +1,8 @@
+main :: IO ()
+main = do
+    print ("Hello")
+
+--Higher order functions take in another function as an argument
+--Higher order function, it's applied to x in its definition
+app :: (a -> b) -> a -> b
+app f x = f x 
