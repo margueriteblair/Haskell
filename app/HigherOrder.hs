b104327d1d05c60d59a2d7b47894925d2ace2061
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-24 17:31:13 -0500

    map higher order function

--- app/HigherOrder.hs
+++ app/HigherOrder.hs
@@ -6,3 +6,15 @@ main = do
 --Higher order function, it's applied to x in its definition
 app :: (a -> b) -> a -> b
 app f x = f x 
+
+--Anonymous functions
+--We don't necessarily want to define functions with a name all the time
+--add1 below can also be written without a named
+add1 :: Int -> Int
+add1 = (\x -> x+1)
+
+--map function maps one list of this type a to another list of this type b
+--The first argument is a function and converts one list to the other
+--These list types can change!
+map :: (a -> b) -> [a] -> [b]
+map (\n -> x+1) [1,2,3,4,5] => [2,3,4,5,6]
