33fb66860f5cbbe4edde1aeafee6502843775d9e
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-25 21:11:17 -0500

    now using our new expr data type to make an evaluate function

--- app/Monads.hs
+++ app/Monads.hs
@@ -5,4 +5,15 @@ main = do
 --Monads are a mathematical concept that originated in the 1960's
 --Gives you a new way of programming with effects
 
+
+--We've got two constructors here in our data type, we've got Val and Div
+--Val builds expressions from integers
+--Div builds expressions from two sub expressions
+--Here we're building a new datatype called Expr 
+--It's got two new constructors, Val & Div
 data Expr = Val Int | Div Expr Expr
+
+--Maths vs Haskell
+eval :: Expr -> Int
+eval (Val n) = n
+eval (Div x y) = eval x `div` eval y
