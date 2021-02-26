5ac9c80086cfc06b8985c0dfc1fe546943e39f17
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-25 21:14:32 -0500

    a few more notes on monads

--- app/Monads.hs
+++ app/Monads.hs
@@ -16,4 +16,9 @@ data Expr = Val Int | Div Expr Expr
 --Maths vs Haskell
 eval :: Expr -> Int
 eval (Val n) = n
-eval (Div x y) = eval x `div` eval y
+eval (Div x y) = eval x `div` eval y --this is problematic though, because y could result to 0 and then we'd have a runtime error
+
+--NB: `div` function performs integer division
+--The (/) function requires arguments whose type is in the class Fractional and performs standard division
+--The div function requires arguments whose type is in the class Integral and performs Integer division
+
