5200b3f8c8b33034e18dde0fc6dd5b4bf919ed05
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-02 23:05:38 -0500

    learn you a haskell bitch

--- LearnYouAHaskell/Main.hs
+++ LearnYouAHaskell/Main.hs
@@ -5,3 +5,6 @@ main = do
 
 --Haskell is lazy, has type inference, and is purely functional
 --Functions in Haskell never consult memory or have side effects
+--When we say that Haskell is lazy, we mean that Haskell will wait until
+--it's absolutely necessary to evaluate functions
+--Haskell has type inference, meaning things are statically typed, but we don't have to declare the type
