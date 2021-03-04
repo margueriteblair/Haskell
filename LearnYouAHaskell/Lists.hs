8b279b2be84382ef10c235ebfd6d8bbbc0fc527c
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-03 20:37:01 -0500

    lists

--- LearnYouAHaskell/Lists.hs
+++ LearnYouAHaskell/Lists.hs
@@ -15,7 +15,20 @@ main = do
 --elem
 
 --list comprehensions
-let nums1 = [2^n | n <- [1..10]]
-let sodaSizes = [2^n | n <- [1..10], 2^n >= 10, 2^n < 100]
-let stringComp = [x | x <- "outrageous", not (elem x "aeiou")]
+nums1 = [2^n | n <- [1..10]]
+sodaSizes = [2^n | n <- [1..10], 2^n >= 10, 2^n < 100]
+stringComp = [x | x <- "outrageous", not (elem x "aeiou")]
 -- Infix method of the above: [x | x <- "outrageous", not (x `elem` "aeiou")]
+
+--Tuples
+--For lists, we can compare two lists of different lengths
+--The size of a tuple is part of its type. We cannot compare tuples of different sizes.
+--All elements of a list must be of the same type, all elements of a tuple can be different.
+
+
+numbers2 = [1..8]
+words = ["one", "two", "three", "four", "five", "six", "seven", "eight"]
+
+
+[(fst p, fst q) | p <- pairs, q <-pairs)]
+[(fst p, fst q) | p <- pairs, q <-pairs, fst p < fst q]
