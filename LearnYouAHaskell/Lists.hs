1905e10ff797bb06be42d7b8137d219847dc0ee3
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-03 19:32:02 -0500

    lists

---
+++ LearnYouAHaskell/Lists.hs
@@ -0,0 +1,5 @@
+--Each element in a linked list has a number and then a reference pointer that points to the next node
+let numbers = [4,8,15,16,23,43]
+    main :: IO ()
+    main = do
+        print (head numbers)
