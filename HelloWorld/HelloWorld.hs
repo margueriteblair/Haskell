8af96c1fab481d40a38d6dabbae8389b674edced
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-03 21:55:26 -0500

    notes on what the contract data type is and the arguments that are being passed in

--- HelloWorld/HelloWorld.hs
+++ HelloWorld/HelloWorld.hs
@@ -5,10 +5,17 @@ import Playground.Contract
 
 --This contract is very simple, and will just log 'Hello, World'
 
+
+--Contracts are the logic that we write for our plutus app to handle transactions between wallets
+--'Contract' is the datatype here.
+--BlockchainActions and T.Text () are parameters for 'Contract'
+--Contract is a generic data structure that takes in 3 types:
+    --BlockchainActions, T.Text, ()
+    --The Contract type is within Language.Plutus.Contract.Types
 hello :: Contract BlockchainActions T.Text ()
 hello = logInfo @String "Hello, world"
 
-endpoints :: Contract BlockchainActions T.Text()
+endpoints :: Contract BlockchainActions T.Text ()
 endpoints = hello
 
 mkSchemaDefinitions ``BlockchainActions
