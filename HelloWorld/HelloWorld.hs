cfd7e3ef8a4147ab91fc6d32c31de004ea60ce8c
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-03 21:36:41 -0500

    hello world

---
+++ HelloWorld/HelloWorld.hs
@@ -0,0 +1,6 @@
+import qualified Data.Text as T
+import Language.Plutus.Contract hiding (when)
+import Language.PlutusTx.Prelude
+import Playground.Contract
+
+--This contract is very simple, and will just log 'Hello, World'
