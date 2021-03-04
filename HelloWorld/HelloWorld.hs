fd45b10138c6f0387dfd37a28b9c53a09a12c742
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-03 22:36:22 -0500

    more notes

--- HelloWorld/HelloWorld.hs
+++ HelloWorld/HelloWorld.hs
@@ -12,6 +12,14 @@ import Playground.Contract
 --Contract is a generic data structure that takes in 3 types:
     --BlockchainActions, T.Text, ()
     --The Contract type is within Language.Plutus.Contract.Types
+    --Contract takes in 3 data types: s e a 
+    --s is the schema
+    --s will return either an error, or another data type which we will dictate
+    --The () are Haskell's way of saying void
+--A schema is a combined list of other classes that contains effects such as producing transactions, collecitng inpus from users, and watching the blockchain for changes
+--Our 'BlockchainActions' schema will help produce txs and collect inputs from users
+--BlockchainActions is a default schema included in Plutus
+
 hello :: Contract BlockchainActions T.Text ()
 hello = logInfo @String "Hello, world"
 
