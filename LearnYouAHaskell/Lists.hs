e88191b4123f012fc48b3eef1baf6a0dc4887e08
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-03 19:43:58 -0500

    manipulating lists

--- LearnYouAHaskell/Lists.hs
+++ LearnYouAHaskell/Lists.hs
@@ -3,6 +3,13 @@ main :: IO ()
 main = do
     let numbers = 5 : 4 : 3 : 2 : 1 : []
     print (length numbers)
+    print (sum [1, 3..99]) --this is called a texas range
+    print (['a' .. 'e'])
 
 --Haskell data structures are immutable and never change
-
+--max, min
+--head, tail
+--last, init
+--sum 
+--product
+--elem
