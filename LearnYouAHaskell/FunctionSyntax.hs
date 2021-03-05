38a44c1b7b0da443a117f113543f3e90372875e1
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-05 12:56:18 -0500

    comments and a few edits on the second increasing function

--- LearnYouAHaskell/FunctionSyntax.hs
+++ LearnYouAHaskell/FunctionSyntax.hs
@@ -2,7 +2,11 @@ main :: IO ()
 main = do
     print ("Hello")
 
+-- For our first instrance of increasing function, our type signature
+-- tells us that for any ordered type a, this function takes a list of a's and returns a bool.
 increasing :: (Ord a) => [a] -> Bool
+--The actual logic of the function is a deeply nested if else statement
+-- We have to check first if our list is empty or if it's just one item bc then it's increasing
 increasing xs = if xs == []
     then True
     else if tail xs == []
@@ -12,7 +16,7 @@ increasing xs = if xs == []
             else False
 
 --the above function can be more easily written using pattern matching syntax
-increasing :: (Ord a) => [a] -> Bool
-increasing [] = True
-increasing [x] = True
-increasing (x : y : ys) = x <= && increasing (y:ys)
+increasing2 :: (Ord a) => [a] -> Bool
+increasing2 [] = True --Again, we say that this empty list is increasing
+increasing2 [x] = True --As is a list of one item
+increasing2 (x : y : ys) = x <= y && increasing (y:ys)
