bd19753b0a8d357a304e0aaea89871e06527262f
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-04 22:33:51 -0500

    types

---
+++ LearnYouAHaskell/Types.hs
@@ -0,0 +1,6 @@
+--Types are sets of values eg Integer
+--Type classes are sets of types eg Num
+--Typeclass -> Type -> Value
+-- :t to find the type of a value
+-- You can get the types of functions as well
+--Sometimes with lazy eval we don't get a specific type, we get the type class
