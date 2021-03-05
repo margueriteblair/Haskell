a4b06c951c9ea82924bc9026b50f9ebd0bd7124b
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-05 15:16:22 -0500

    made the entire game board

--- LearnYouAHaskell/TicTacToe.hs
+++ LearnYouAHaskell/TicTacToe.hs
@@ -3,4 +3,12 @@ emptyBoard = ["...", "...", "..."]
 --Returns the winner
 winner :: [[Char]] -> Char
 winner [[a,b,c], [d,e,f], [g,h,i]]
-    | a == b && b == c && a /= '. = a
+    | a == b && b == c && a /= "."   = a
+    | d == e && e == f && d /= "."   = d
+    | g == h && h == i && h /= "."   = g
+    | b == e && e == h && b /= "."   = b
+    | c == f && f == i && c /= "."   = c
+    | a == e && e == i && a /= "."   = a
+    | c == e && c == g && c /= "."   = c
+    | '.' `elem` [a,b,c,d,e,f,g,h,i] = '?' --Game not over
+    | otherwise                      = '-' --Tie                  
