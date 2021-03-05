01169760fc0240082526c463ea2f50124cd731b1
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-04 22:47:05 -0500

    pattern matching version of increasing

--- LearnYouAHaskell/FunctionSyntax.hs
+++ LearnYouAHaskell/FunctionSyntax.hs
@@ -9,4 +9,10 @@ increasing xs = if xs == []
         then True
         else if head xs <= head (tail xs)
             then increasing (tail xs)
-            else False;
+            else False
+
+--the above function can be more easily written using pattern matching syntax
+increasing :: (Ord a) => [a] -> Bool
+increasing [] = True
+increasing [x] = True
+increasing (x : y : ys) = x <= && increasing (y:ys)
