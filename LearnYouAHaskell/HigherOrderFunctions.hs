d7806d992fe86b85ad3dfedd58b1140629d09a4f
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-07 20:55:39 -0500

    map

--- LearnYouAHaskell/HigherOrderFunctions.hs
+++ LearnYouAHaskell/HigherOrderFunctions.hs
@@ -1,3 +1,7 @@
+main :: IO ()
+main = do
+    print (map add1 [1, 2, 3, 4])
+
 add1 :: Int -> Int
 add1 x = x + 1
 
@@ -7,4 +11,4 @@ f x = 3
 g :: Int -> (Int -> Int)
 g x = add1
 
-add1ToEach :: [Int] -> [Int]
+-- add1ToEach :: [Int] -> [Int]
