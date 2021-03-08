81ab00495377cf7e8ed53b45c43cf783ca1bb554
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-07 23:15:23 -0500

    have to find a way to make it recursive

--- OceanView.hs
+++ OceanView.hs
@@ -1,10 +1,13 @@
 main :: IO ()
 main = do
-    print (oceanFront [4,3,2,3,1])
+    print (oceanFront [4,3,2,3,1] 0 [])
 
 
-oceanFront :: [Int] -> [Int]
-oceanFront xs =
-    --we have a variable big = 0
-    --if last x > big, then we allow x in the array and big is set to x
-    --Then we go again with the first elements of the array
+oceanFront :: [Int] -> Int -> [Int] -> [Int]
+oceanFront xs e arr = 
+    if last xs > e
+        then (last xs) : arr
+    else 0 : arr
+--we have a variable big = 0
+--if last x > big, then we allow x in the array and big is set to x
+--Then we go again with the first elements of the array
