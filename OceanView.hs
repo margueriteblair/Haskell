31d55a6eaea0f659fb893eab4ae1c32a1f9a24d2
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-08 19:18:01 -0500

    changes

--- OceanView.hs
+++ OceanView.hs
@@ -1,13 +1,11 @@
 main :: IO ()
 main = do
-    print (oceanFront [4,3,2,3,1] 0 [])
+    print (oceanFront [4,3,2,3,1] 0)
 
 
-oceanFront :: [Int] -> Int -> [Int] -> [Int]
-oceanFront xs e arr = 
-    if last xs > e
-        then (last xs) : arr
-    else 0 : arr
+oceanFront :: [Int] -> Int -> [Int]
+oceanFront xs e = 
+    [ x | x <- xs, x > e]
 --we have a variable big = 0
 --if last x > big, then we allow x in the array and big is set to x
 --Then we go again with the first elements of the array
