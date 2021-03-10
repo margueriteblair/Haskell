6de8fe20615896191b44238a21de35ecad36835b
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-09 21:52:56 -0500

    the rest of the starter code before being fleshed out and customized

--- Endpoints/Starter.hs
+++ Endpoints/Starter.hs
@@ -7,3 +7,62 @@ import qualified Ledger.Constraints        as Constraints
 import qualified Ledger.Typed.Scripts      as Scripts
 import           Ledger.Value              (Value)
 import           Playground.Contract
+
+newtype MyDatum = MyDatum Integer deriving newtype PlutusTx.IsData
+PlutusTx.makeLift ''MyDatum
+
+newtype MyRedeemer = MyRedeemer Integer deriving newtype PlutusTx.IsData
+PlutusTx.makeLift ''MyRedeemer
+
+-- | This method is the spending validator (which gets lifted to
+--   its on-chain representation).
+validateSpend :: MyDatum -> MyRedeemer -> ValidatorCtx -> Bool
+validateSpend _myDataValue _myRedeemerValue _ = error () -- Please provide an implementation.
+
+-- | The address of the contract (the hash of its validator script).
+contractAddress :: Address
+contractAddress = Ledger.scriptAddress (Scripts.validatorScript starterInstance)
+
+data Starter
+instance Scripts.ScriptType Starter where
+    type instance RedeemerType Starter = MyRedeemer
+    type instance DatumType Starter = MyDatum
+
+-- | The script instance is the compiled validator (ready to go onto the chain)
+starterInstance :: Scripts.ScriptInstance Starter
+starterInstance = Scripts.validator @Starter
+    $$(PlutusTx.compile [|| validateSpend ||])
+    $$(PlutusTx.compile [|| wrap ||]) where
+        wrap = Scripts.wrapValidator @MyDatum @MyRedeemer
+
+-- | The schema of the contract, with two endpoints.
+type Schema =
+    BlockchainActions
+        .\/ Endpoint "publish" (Integer, Value)
+        .\/ Endpoint "redeem" Integer
+
+contract :: AsContractError e => Contract Schema e ()
+contract = publish `select` redeem
+
+-- | The "publish" contract endpoint.
+publish :: AsContractError e => Contract Schema e ()
+publish = do
+    (i, lockedFunds) <- endpoint @"publish"
+    let tx = Constraints.mustPayToTheScript (MyDatum i) lockedFunds
+    void $ submitTxConstraints starterInstance tx
+
+-- | The "redeem" contract endpoint.
+redeem :: AsContractError e => Contract Schema e ()
+redeem = do
+    myRedeemerValue <- endpoint @"redeem"
+    unspentOutputs <- utxoAt contractAddress
+    let redeemer = MyRedeemer myRedeemerValue
+        tx       = collectFromScript unspentOutputs redeemer
+    void $ submitTxConstraintsSpending starterInstance unspentOutputs tx
+
+endpoints :: AsContractError e => Contract Schema e ()
+endpoints = contract
+
+mkSchemaDefinitions ''Schema
+
+$(mkKnownCurrencies [])
