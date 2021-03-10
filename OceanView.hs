0a5e1038c37e3c5d4d63cb657f4f14a6c90dc194
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-10 00:03:41 -0500

    starting over with pattern matching for the two easy base cases

--- OceanView.hs
+++ OceanView.hs
@@ -1,11 +1,12 @@
 main :: IO ()
 main = do
-    print (oceanFront [4,3,2,3,1] 0)
+    print (oceanFront [4,3,2,3,1])
 
 
-oceanFront :: [Int] -> Int -> [Int]
-oceanFront xs e = 
-    [ x | x <- xs, x > e]
+oceanFront :: [Int] -> [Int]
+oceanFront [] = []
+oceanFront [x] = [x]
+
 --we have a variable big = 0
 --if last x > big, then we allow x in the array and big is set to x
 --Then we go again with the first elements of the array
