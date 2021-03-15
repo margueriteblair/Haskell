250b9e89e3f747ac9470a00f4a8695bdef652f59
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-14 22:58:41 -0400

    a bunch o functions

--- LearnYouAHaskell/Lambdas.hs
+++ LearnYouAHaskell/Lambdas.hs
@@ -2,4 +2,24 @@ main :: IO ()
 main = do
     print (map (\x -> x + 1) [4, 5, 6, 7])
 
+addAll :: (Num a) => [a] -> a
+addAll [] = 0
+addAll (x:xs) = x + addAll xs
+
+multAll :: (Num a) => [a] -> a
+multAll [] = 1
+multAll (x:xs) = x * multAll xs
+
+firstThat :: (a -> Bool) -> a -> [a] -> a
+firstThat f = foldr (\x acc -> if f x then x else acc)
+
+lastThat :: (a -> Bool) -> a -> [a] -> a
+lastThat f = foldl (\acc x -> if f x then x else acc)
+
+argmax :: (Ord b) => (a -> b) -> [a] -> a
+argmax f[x] = x
+argmax f (x:xs) = if f x > f (argmax f xs)
+                  then x
+                  else (argmax f xs)
+
 
