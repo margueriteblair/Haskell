52c751c4c80a6dab800909ce3bfd90708d2e30c0
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-14 23:34:22 -0400

    new versions of mult and add all with the fold functions in place

--- LearnYouAHaskell/Lambdas.hs
+++ LearnYouAHaskell/Lambdas.hs
@@ -2,13 +2,22 @@ main :: IO ()
 main = do
     print (map (\x -> x + 1) [4, 5, 6, 7])
 
+
+--equivalent of sum
 addAll :: (Num a) => [a] -> a
 addAll [] = 0
 addAll (x:xs) = x + addAll xs
 
+addAll2 :: (Num a) => [a] -> a
+addAll2 = foldl (+) 0
+
+--Equivalent of product
 multAll :: (Num a) => [a] -> a
 multAll [] = 1
 multAll (x:xs) = x * multAll xs
+
+multAll2 :: (Num a) => [a] -> a
+multAll2 = foldl (*) 1
 
 firstThat :: (a -> Bool) -> a -> [a] -> a
 firstThat f = foldr (\x acc -> if f x then x else acc)
