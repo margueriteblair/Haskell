9f28ee488bc272cf1a9c812b593f45d6e1d0d0cc
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-15 23:21:48 -0400

    fleshing out the cards and then how each pllayer will have a hand

--- LearnYouAHaskell/DataTypes.hs
+++ LearnYouAHaskell/DataTypes.hs
@@ -1,8 +1,22 @@
 import Data.List ()
 
+
+{- Rank is an instance of 'Eq' meaning that we can check two
+ranks for equality.
+
+-}
 data Rank = Ace | Two | Three | Four | Five | Six | Seven
             | Eight | Nine | Ten | Jack | Queen | King
             deriving (Eq, Ord, Bounded, Enum, Show, Read)
 
 data Suit = Spades | Hearts | Diamonds | Clubs
             deriving (Eq, Enum, Show, Read)
+
+data Card = Card Rank Suit derving (Eq, Show, Read)
+
+type Hand = [Card]
+
+--Returns True if all consecutive pair satisfy f
+allPairs f [] = True
+allPairs f [x] = True
+allPairs f (x:y:ys) = f x y && allPairs f (y:ys)
