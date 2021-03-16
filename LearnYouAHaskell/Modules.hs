a452db37e97338fa7abafcd93580ed4f9f4c0ca4
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-15 22:04:43 -0400

    quit

--- LearnYouAHaskell/Modules.hs
+++ LearnYouAHaskell/Modules.hs
@@ -3,3 +3,9 @@
 --There are many built in modules
 --Modules are things like Data.List, Data.Char, Data.Map, Data.Set
 --When we log into ghci, the Prelude module is what shows up by default
+--We can load other modules directly in ghci with :m + <MOMDULE>
+-- :m + Data.List
+-- find (>3) [1..6] maybe it returns a value
+-- In an actual file as opposed to ghci, we can import our modules up at the top like so:
+import Data.List
+import Data.Maybe
