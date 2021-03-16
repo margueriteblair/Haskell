767d0dafed825f9f20c118a65fcc40bc77cfc5a1
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-15 23:19:31 -0400

    added the suit as well

--- LearnYouAHaskell/DataTypes.hs
+++ LearnYouAHaskell/DataTypes.hs
@@ -1,5 +1,8 @@
-import Data.List
+import Data.List ()
 
 data Rank = Ace | Two | Three | Four | Five | Six | Seven
             | Eight | Nine | Ten | Jack | Queen | King
             deriving (Eq, Ord, Bounded, Enum, Show, Read)
+
+data Suit = Spades | Hearts | Diamonds | Clubs
+            deriving (Eq, Enum, Show, Read)
