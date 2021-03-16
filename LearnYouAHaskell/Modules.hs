669d94c515c99369e6609d27c634c9214b67cfb3
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-15 22:01:58 -0400

    modules overview

---
+++ LearnYouAHaskell/Modules.hs
@@ -0,0 +1,5 @@
+--Haskell functions can be grouped into modules.
+--A Haskell module is somewhere between a class and a package in Java
+--There are many built in modules
+--Modules are things like Data.List, Data.Char, Data.Map, Data.Set
+--When we log into ghci, the Prelude module is what shows up by default
