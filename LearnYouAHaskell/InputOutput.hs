465a7ec398452576484b6f7711c148a8fc146289
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-17 22:10:49 -0400

    second draft with random

--- LearnYouAHaskell/InputOutput.hs
+++ LearnYouAHaskell/InputOutput.hs
@@ -1,7 +1,16 @@
 import System.IO
+import System.Random
 
 main = do
+    --openFile returns an IO file wrapped around a handle
+    --We store this in the variable handle
     handle <- openFile "enable1.txt" ReadMode
+    -- --hGesContents
     contents <- hGetContents handle
-    putStrLn ("There are " ++ show (length (lines contents)) ++ " words")
+    gen <- getStdGen
+    let words = map init (lines contents)
+        (n, _) = randomR (0, (length words) - 1) gen :: (Int, StdGen)
+    play (words !! n)
     hClose handle
+    -- putStrLn ("There are " ++ show (length (lines contents)) ++ " words")
+    -- hClose handle
