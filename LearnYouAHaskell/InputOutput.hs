56a56f7bbf0b0e38084d54638b3a73d5b17248c4
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-17 21:57:38 -0400

    starting the discussion on inpout output

---
+++ LearnYouAHaskell/InputOutput.hs
@@ -0,0 +1,7 @@
+import System.IO
+
+main = do
+    handle <- openFile "enable1.txt" ReadMode
+    contents <- hGetContents handle
+    putStrLn ("There are " ++ show (length lines contents)) ++ " words")
+    hClose handle
