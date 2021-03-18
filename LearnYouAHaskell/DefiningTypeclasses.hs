5451e0f661514ffe74ca27ffe399d033f6acc43b
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-17 21:25:00 -0400

    were also making quad of type show so that it prints out in a very specific way

--- LearnYouAHaskell/DefiningTypeclasses.hs
+++ LearnYouAHaskell/DefiningTypeclasses.hs
@@ -4,4 +4,11 @@ import Data.Maybe
 --Quad here will represent a 2D matrix
 --In this case, think of Quad as a generic in Java
 --'a' is a type parameter
+--Quad is a type constructor
+--the second 'Quad' we see on line 9 is a constructor, that takes 4 arguments, all of type 'a'
+--when we say type 'a' these are all the same parts, like integers :)
 data Quad a = Quad a a a a
+
+instance (Show a) => Show (Quad a) where
+        show (Quad a b c d) = (show a) ++ " " ++ (show b) ++ "\n"
+                              (show c) ++ " " ++ (show d)
