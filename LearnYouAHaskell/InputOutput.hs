2d80b6f7f2c25d5eaea7bca9eb4048cca01b731c
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-17 23:04:10 -0400

    hangman

--- LearnYouAHaskell/InputOutput.hs
+++ LearnYouAHaskell/InputOutput.hs
@@ -10,10 +10,18 @@ main = do
     contents <- hGetContents handle
     gen <- getStdGen
     let words = map init (lines contents)
-        (n, _) = randomR (0, (length words) - 1) gen :: (Int, StdGen)
+        (n, _) = randomR (0, (length words) - 1) gen :: (Int, StdGen) --We need a type specifiction
     play (words !! n)
     hClose handle
     -- putStrLn ("There are " ++ show (length (lines contents)) ++ " words")
     -- hClose handle
 play word = do
-    putStrLn ("The word is " ++ word ++ ".")
+    putStrLn word
+    putStrLn "Enter a letter to guess: "
+    line <- getLine
+    putStrLn (handle (head line) word)
+    play word
+
+handle letter word
+    | letter `elem` word = "Yup"
+    | otherwise = "Nope"
