2b064c11ecc8816d3486e69a7b38272d85e21517
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-17 21:19:28 -0400

    setting up quad

---
+++ LearnYouAHaskell/DefiningTypeclasses.hs
@@ -0,0 +1,7 @@
+import Data.List
+import Data.Maybe
+
+--Quad here will represent a 2D matrix
+--In this case, think of Quad as a generic in Java
+--'a' is a type parameter
+data Quad a = Quad a a a a
