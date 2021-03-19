611a7550f22664fa2052ff5816036356a9c96190
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-18 23:22:59 -0400

    valid and apply functions

--- Nottingham/Countdown.hs
+++ Nottingham/Countdown.hs
@@ -1,7 +1,25 @@
-import System.IO
-import System.CPUTime
-import Numeric
+import System.IO --input output lib
+import System.CPUTime --timing lib
+import Numeric --numeric library
 
 --Arithmetic operators
 
 data Op = Add | Sub | Mul | Div
+
+instance Show Op where
+    show Add = "+"
+    show Sub = "-"
+    show Mul = "*"
+    show Div = "/"
+
+valid :: Op -> Int -> Int -> Bool
+valid Add _ _ = True
+valid Sub x y = x > y
+valid Mul _ _ = True
+valid Div x y = x `mod` y == 0
+
+apply :: Op -> Int -> Int -> Int
+apply Add x y = x + y
+apply Sub x y = x - y
+apply Mul x y = x * y
+apply Div x y = x `div` y
