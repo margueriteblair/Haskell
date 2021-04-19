d04228eee342b18f05cb5389c8ebadbf8e8b8b94
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-19 14:58:22 -0400

    person 2 is made with haskell record syntax

--- LearnYouAHaskell/RecordSyntax.hs
+++ LearnYouAHaskell/RecordSyntax.hs
@@ -2,4 +2,14 @@ import System.IO
 
 data Person = Person String String String Int Float String String deriving (Show)
 
---This is kind of unreadable
+--This is kind of unreadable, and it's a pain to return single values of any part of the data
+
+--We can achieve the same as the above with more readability
+data Person2 = Person2 {
+    firstName :: String,
+    lastName :: String,
+    age :: Int,
+    height :: Float,
+    phoneNumber :: String,
+    flavor :: String
+} deriving (Show)
