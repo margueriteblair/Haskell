4b45a97f42ebf93174ebc8f1cc914b1108daaa5f
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-06 16:45:51 -0400

    writer

--- plutus-pioneer-program/code/week04/Writer.hs
+++ plutus-pioneer-program/code/week04/Writer.hs
@@ -1,32 +1,54 @@
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE DataKinds        #-}
+module Week04.Writer where
 
-module Week04.Trace where
+import Control.Monad
+-- import Week04.Monad unnecessary to compile
 
-import Control.Monad.Freer.Extras as Extras
-import Data.Functor               (void)
-import Ledger
-import Plutus.Trace.Emulator      as Emulator
-import Wallet.Emulator.Wallet
+--Making a data type Writer, takes two args, a and a list of log messages
+data Writer a = Writer a [String]
+    deriving Show
 
-import Week04.Vesting
+number :: Int -> Writer Int --Project writer int onto this
+number n = Writer n $ ["number: " ++ show n]
 
--- Contract w s e a
--- EmulatorTrace a
+tell :: [String] -> Writer () --gets a list of log messages and produces writer unit
+tell = Writer ()
 
-test :: IO ()
-test = runEmulatorTraceIO myTrace
+foo :: Writer Int -> Writer Int -> Writer Int -> Writer Int
+foo (Writer k xs) (Writer l ys) (Writer m zs) =
+  let --let in convention
+    s = k + l + m
+    Writer _ us = tell ["sum: " ++ show s]
+  in
+    Writer s $ xs ++ ys ++ zs ++ us
 
-myTrace :: EmulatorTrace ()
-myTrace = do
-    h1 <- activateContractWallet (Wallet 1) endpoints
-    h2 <- activateContractWallet (Wallet 2) endpoints
-    callEndpoint @"give" h1 $ GiveParams
-        { gpBeneficiary = pubKeyHash $ walletPubKey $ Wallet 2
-        , gpDeadline    = Slot 20
-        , gpAmount      = 1000
-        }
-    void $ waitUntilSlot 20
-    callEndpoint @"grab" h2 ()
-    s <- waitNSlots 1
-    Extras.logInfo $ "reached slot " ++ show s
+bindWriter :: Writer a -> (a -> Writer b) -> Writer b
+bindWriter (Writer a xs) f =
+  let
+    Writer b ys = f a
+  in
+    Writer b $ xs ++ ys
+
+foo' :: Writer Int -> Writer Int -> Writer Int -> Writer Int
+foo' x y z = x `bindWriter` \k ->
+             y `bindWriter` \l ->
+             z `bindWriter` \m ->
+             let s = k + l + m
+             in tell ["sum: " ++ show s] `bindWriter` \_ ->
+                Writer s []
+
+foo'' :: Writer Int -> Writer Int -> Writer Int -> Writer Int
+foo'' x y z = do
+    s <- threeInts x y z
+    tell ["sum: " ++ show s]
+    return s
+
+instance Functor Writer where
+    fmap = liftM
+
+instance Applicative Writer where
+    pure = return
+    (<*>) = ap
+
+instance Monad Writer where
+    return a = Writer a []
+    (>>=) = bindWriter
