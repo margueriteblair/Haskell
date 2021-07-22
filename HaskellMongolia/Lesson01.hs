b2cf6a7a217edc40900095d582d3f540be2418e3
Marguerite Blair <marguerite.blair@gmail.com>
2021-07-21 22:39:36 -0400

    haskell bindings

--- HaskellMongolia/Lesson01.hs
+++ HaskellMongolia/Lesson01.hs
@@ -4,3 +4,12 @@ data Chain =
      GenesisBlock
     | Block Chain Txs
 type Txs = Int
+
+--This is actually recursive, because we pass Chain into the definition of Block datatype
+-- And we're defining chain here
+
+chain1 =
+    Block GenesisBlock 2
+
+chain2 =
+    Block chain1 4
