cb4771123cbd2fe2b4bac515f2c4c359d1e8e058
Marguerite Blair <marguerite.blair@gmail.com>
2021-07-21 23:43:26 -0400

    has Gensis function to return boolean if a chain has a block with a given value

--- HaskellMongolia/Lesson01.hs
+++ HaskellMongolia/Lesson01.hs
@@ -25,3 +25,9 @@ chain2' =
 chainLength :: Chain -> Int
 chainLength GenesisBlock = 0
 chainLength (Block c _)  = chainLength c + 1
+
+
+--hasBlock tests whether a given chain has a block w/ a particular value
+hasBlock x GenesisBlock = False
+hasBlock x (Block c t) =
+    x == t || hasBlock x c
