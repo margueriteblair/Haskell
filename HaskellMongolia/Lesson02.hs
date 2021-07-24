70f89ccbaeb88f3e5168946ffaa1d2938777f3ea
Marguerite Blair <marguerite.blair@gmail.com>
2021-07-23 23:28:47 -0400

    function that is higher order to determine whether or not a block has a given property

--- HaskellMongolia/Lesson02.hs
+++ HaskellMongolia/Lesson02.hs
@@ -5,3 +5,11 @@ containsBlock :: Int -> Chain -> Bool
 containsBlock GensisBlock = False
 containsBlock x (Block c t) =
     t == x || containsBlock x c
+
+--Remember, Txs is just a type synonym for Integer,
+-- That being said, we can make a function that'll determine whether this Tx/Int meets a given criteria
+
+hasBlockProp :: (Txs -> Bool) -> Chain -> Bool
+hasBlockProp prop GensisBlock = False
+hasBlockProp prop (Block chain tx) =
+    prop t || hasBlockProp prop c
