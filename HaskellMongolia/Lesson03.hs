bea99b349120c359b1d7645b25ce543c3bd24af6
Marguerite Blair <marguerite.blair@gmail.com>
2021-07-29 09:33:08 -0400

    notes on preloaded modules in ghci

---
+++ HaskellMongolia/Lesson03.hs
@@ -0,0 +1,11 @@
+import System.IO 
+{-
+In this lecture, we introduce the standard design pattern (catamorphism) which shapes the function
+cata-means down
+Bool is predefined, but not BUILT into Haskell
+
+In Haskell, there are standard functions that aren't built into the language,
+but instead are in a default module that's always loaded when you start GhCI or when you write a module by yourself
+This module is called the prelude
+
+-}
