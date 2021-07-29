bf21c680a35f6ddf268a514d4f5ea9f1b59d75e9
Marguerite Blair <marguerite.blair@gmail.com>
2021-07-29 10:04:37 -0400

    notes on pattern matching and such

--- HaskellMongolia/Lesson03.hs
+++ HaskellMongolia/Lesson03.hs
@@ -9,3 +9,17 @@ but instead are in a default module that's always loaded when you start GhCI or 
 This module is called the prelude
 
 -}
+
+--We can also import booleans manually without the Prelude:
+data Bool = True | False deriving Show
+
+{-
+Allowed patterns are:
+Saturated constructor applications to other patterns.
+Variables (match anything)
+Underscore / wildcard pattern
+Literals (numbers, chars, strings, list)
+For non-overlapping definitions, the order does not matter
+    - this means that if the cases are distinct, there's no overlapping qualitites, then the order doesn't matter
+Best practice is to split cases into different constructors
+-}
