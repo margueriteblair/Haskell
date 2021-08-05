d8a60ec6f3a3f9974125e71bf37a7954af7fa1c0
Marguerite Blair <margueriteblair@Marguerites-MacBook-Pro-2.local>
2021-08-04 22:44:12 -0400

    interface to pair with the table type

--- HaskellMongolia/Lesson04.hs
+++ HaskellMongolia/Lesson04.hs
@@ -4,3 +4,8 @@ import System.IO
 type Table k v = [(k, v)] --like key, value we want to associate keys & values
 --We can achieve this with a type synonym for lists of pairs
 --notes to test on other comp
+--For this lookup table type, we'd like an interface of available functions so we can work with this type:
+empty :: Table k v
+insert :: k -> v -> Table k v -> Table k v 
+delete :: Eq k => k -> Table k v -> Table k v
+lookup :: Eq k => k -> Table k v -> Maybe v 
