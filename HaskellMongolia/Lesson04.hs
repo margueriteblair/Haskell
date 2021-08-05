d49942240a83061ef998c464a43dbadf0a071a43
Marguerite Blair <margueriteblair@Marguerites-MacBook-Pro-2.local>
2021-08-04 22:29:41 -0400

    writing a type table that is synonymous with lists of pairs

--- HaskellMongolia/Lesson04.hs
+++ HaskellMongolia/Lesson04.hs
@@ -1,3 +1,6 @@
 import System.IO
 
+
+type Table k v = [(k, v)] --like key, value we want to associate keys & values
+--We can achieve this with a type synonym for lists of pairs
 --notes to test on other comp
