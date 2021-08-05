49682689945b1f11a588af6206eded32353f369d
Marguerite Blair <margueriteblair@MargueritesMBP2.fios-router.home>
2021-08-05 01:56:28 -0400

    making edits to delete func

--- HaskellMongolia/Lesson04.hs
+++ HaskellMongolia/Lesson04.hs
@@ -9,10 +9,14 @@ empty :: Table k v --a method to construct an empty table
 empty = []
 insert :: k -> v -> Table k v -> Table k v --method where we take a key, a value, an existing table, returns a new table
 insert k v t = (k, v) : t --just prepend the k and v to the existng table, pairs occuring earlier will shadow the existing one
-delete :: Eq k => k -> v -> Table k v -> Table k v --We will need an equality constaint
-delete _ _ [] = [] --deleting from an empty list pattern matcing
-delete k v ((k', v') : kvs )
+delete :: Eq k => k -> Table k v -> Table k v --We will need an equality constaint
+delete _ [] = [] --deleting from an empty list pattern matcing
+delete k ((k', v) : kvs )
     | k == k' = delete k kvs 
     | otherwise = (k', v') : delete k kvs
+
+
+delete' :: Eq k => k -> Table k v -> Table k v 
+delete' k = filter (\ (k', _) -> not (k == k'))
 -- lookup :: Eq k => k -> Table k v -> Maybe v  --lookup where we need equality to compare. if the key is not found in the table then we return nothing
 --as we can see here, we don't modify existing tables, we create new ones
