a881b736744eeaac3aefd78b6cb51ebdc6916ed8
Marguerite Blair <margueriteblair@MargueritesMBP2.fios-router.home>
2021-08-08 16:39:41 -0400

    height

--- HaskellMongolia/Lesson05.hs
+++ HaskellMongolia/Lesson05.hs
@@ -10,3 +10,8 @@ Node :: Tree a -> Tree a -> Tree a
 flatten :: Tree a -> [a]
 flatten (Leaf a) = [a]
 flatten (Node l r) = flatten l ++ flatten r
+
+--computing the height of a tree
+height :: Tree a -> Int
+height (Leaf x) = 0
+height (Node l r) = 1 + max (height l) (height r)
