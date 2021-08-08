f8f5f74c4056a31002d473a21645e0259e563a2c
Marguerite Blair <margueriteblair@MargueritesMBP2.fios-router.home>
2021-08-08 16:37:14 -0400

    flatten function

--- HaskellMongolia/Lesson05.hs
+++ HaskellMongolia/Lesson05.hs
@@ -7,3 +7,6 @@ data Tree a = Leaf a | Node (Tree a) (Tree a)
 Leaf :: a -> Tree a 
 Node :: Tree a -> Tree a -> Tree a
 
+flatten :: Tree a -> [a]
+flatten (Leaf a) = [a]
+flatten (Node l r) = flatten l ++ flatten r
