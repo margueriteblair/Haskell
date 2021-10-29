1e75fdbecbd397e3c0481f18c812e06c942bcaa7
Marguerite Blair <marguerite.blair@gmail.com>
2021-10-28 23:25:32 -0400

    a few notes about maybe

--- plutus-pioneer-program-copy/Week04/Monads.hs
+++ plutus-pioneer-program-copy/Week04/Monads.hs
@@ -6,3 +6,6 @@ bar = getLine >>= \s ->
     getLine >>= \t ->
         putStrLn (s ++ " " ++ t)
 
+
+data Maybe a = Nothing | Just a
+--Defined in GHC.Maybe, sort of like the Optional operator in Java
