4d1afdb19282ebecb5057aabb0d2199ce50f72df
Marguerite Blair <marguerite.blair@gmail.com>
2021-10-30 00:38:55 -0400

    notes on the bindMaybe helper function when parsing 3 strings to ints and adding them

--- plutus-pioneer-program-copy/Week04/Maybe.hs
+++ plutus-pioneer-program-copy/Week04/Maybe.hs
@@ -14,16 +14,22 @@ foo x y z = case readMaybe x of
             Nothing -> Nothing
             Just m  -> Just (k + l + m)
 
---bind maybe function
+--bind maybe function to make the code above in the "foo" function more resuable
+--this function takes a Maybe a which goes to another function (a -> Maybe a)
 bindMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
 bindMaybe Nothing  _ = Nothing
 bindMaybe (Just x) f = f x
 
+--now we use the bind maybe as a helper function to write foo'
+--This simplifies the foo function so we don't have messy logic
 foo' :: String -> String -> String -> Maybe Int
 foo' x y z = readMaybe x `bindMaybe` \k ->
              readMaybe y `bindMaybe` \l ->
              readMaybe z `bindMaybe` \m ->
              Just (k + l + m)
+-- foo' :: String -> String -> String -> Maybe Int
+-- foo' x y z = readMaybe x `bindMaybe`
+
 
 foo'' :: String -> String -> String -> Maybe Int
 foo'' x y z = threeInts (readMaybe x) (readMaybe y) (readMaybe z)
